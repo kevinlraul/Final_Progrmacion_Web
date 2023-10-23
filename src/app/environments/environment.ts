9a350b17ccd132627a3e7c3cefce2ba3bbdc5efa
kevinlraul <102550380+kevinlraul@users.noreply.github.com>
2023-10-23 00:50:53 -0300

    Delete src directory

--- src/app/environments/environment.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  production: false,
-  /*api_key: '1a78786d', */  
-
-  api_key: 'faf99f2e9d01798939051efe468eb849',
-  url: 'http://localhost:4200/',
-  baseUrl: "http://localhost/final_progra_web/php"
-
-};
